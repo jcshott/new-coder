71ec3a515fa37287e281411dfaf7b0c3694569c9
Corey <jcshott@gmail.com>
2016-01-05 15:46:15 -0800

    some work on class

--- scrape/scrape_workspace/my_scraper/scraper_app/items.py
+++ scrape/scrape_workspace/my_scraper/scraper_app/items.py
@@ -2,3 +2,11 @@ from scrapy.item import Item, Field
 
 class LivingSocialDeal(Item):
 	"""LivingSocial container (dictionary-like object) for scraped data"""
+	title = Field()
+	link = Field()
+	location = Field()
+	original_price = Field()
+	price = Field()
+	end_date = Field()
+
+	
